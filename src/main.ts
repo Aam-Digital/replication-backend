0405866e7143ca265db1c47c38993ba1c2d0cdf1
Simon <therealslimv@yahoo.de>
2021-09-02 11:10:18 +0200

    added swagger package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // SwaggerUI setup see https://docs.nestjs.com/openapi/introduction#bootstrap
+  const config = new DocumentBuilder()
+    .setTitle('Replication Backend')
+    .setDescription('A proxy that implements the CouchDB replication protocol')
+    .setVersion('Beta')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
