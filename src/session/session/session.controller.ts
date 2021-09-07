5d93a1c53d2e5fe12d162e66248f23fd0e48c892
Simon <therealslimv@yahoo.de>
2021-09-07 16:44:22 +0200

    fixed session function to correctly return user object

--- src/session/session/session.controller.ts
+++ src/session/session/session.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { UserCredentials } from './user-auth.dto';
 import { CouchAuthGuard } from '../auth/couch-auth.guard';
 import { ApiBody } from '@nestjs/swagger';
+import { Request } from 'express';
 
 @Controller()
 export class SessionController {
@@ -12,5 +13,7 @@ export class SessionController {
   @ApiBody({ type: UserCredentials })
   @UseGuards(CouchAuthGuard)
   @Post('/_session')
-  session() {}
+  session(@Req() request: Request) {
+    return request.user;
+  }
 }
