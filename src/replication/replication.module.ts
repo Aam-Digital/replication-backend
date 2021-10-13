8e70ab1d1b68bd2794a11f2de8bc2340bc9e5326
Simon <therealslimv@yahoo.de>
2021-10-13 20:25:46 +0200

    added GET endpoint for the rules object

--- src/replication/replication.module.ts
+++ src/replication/replication.module.ts
@@ -5,10 +5,11 @@ import { DocumentFilterService } from './document-filter/document-filter.service
 import { SessionModule } from '../session/session.module';
 import { RulesService } from './rules/rules.service';
 import { PermissionService } from './permission/permission.service';
+import { RulesController } from './rules/rules.controller';
 
 @Module({
   imports: [HttpModule, SessionModule],
-  controllers: [CouchProxyController],
+  controllers: [CouchProxyController, RulesController],
   providers: [DocumentFilterService, RulesService, PermissionService],
 })
 export class ReplicationModule {}
