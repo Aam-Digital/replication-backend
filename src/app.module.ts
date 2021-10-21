5b5ce6ac2e1db4f44968df7445a19e3f4f0e0207
Simon <therealslimv@yahoo.de>
2021-10-21 17:22:28 +0200

    added sentry in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ReplicationModule } from './replication/replication.module';
 import { UserModule } from './user/user.module';
+import { SentryModule } from '@ntegral/nestjs-sentry';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ReplicationModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ReplicationModule,
+    UserModule,
+    SentryModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        dsn: configService.get('SENTRY_DSN'),
+        debug: true,
+        environment: 'prod',
+        release: 'backend@latest',
+        whitelistUrls: [/https?:\/\/(.*)\.?aam-digital\.com/],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
