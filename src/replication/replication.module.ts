dfc74c482ae287e7007a8bb534b350962c959e68
Simon <therealslimv@yahoo.de>
2021-12-06 17:48:29 +0100

    removed rules controller

--- src/replication/replication.module.ts
+++ src/replication/replication.module.ts
@@ -5,11 +5,10 @@ import { DocumentFilterService } from './document-filter/document-filter.service
 import { SessionModule } from '../session/session.module';
 import { RulesService } from './rules/rules.service';
 import { PermissionService } from './permission/permission.service';
-import { RulesController } from './rules/rules.controller';
 
 @Module({
   imports: [HttpModule, SessionModule],
-  controllers: [CouchProxyController, RulesController],
+  controllers: [CouchProxyController],
   providers: [DocumentFilterService, RulesService, PermissionService],
 })
 export class ReplicationModule {}
