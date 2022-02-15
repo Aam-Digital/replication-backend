9221bafc5e209ea59ccdc4e356937fc3804a1cf8
Simon <therealslimv@yahoo.de>
2022-02-15 19:53:00 +0100

    fix: fix regex so it correctly matches documents

--- src/proxy/proxy.module.ts
+++ src/proxy/proxy.module.ts
@@ -29,9 +29,10 @@ export class ProxyModule implements NestModule {
         { path: ':db/_all_docs', method: RequestMethod.POST },
         { path: ':db/_all_docs', method: RequestMethod.GET },
         { path: ':db/clear_local', method: RequestMethod.POST },
-        // First character of ID has to be letter or number, otherwise potential collision with `_changes` request
-        { path: ':db/:docId([A-Za-b0-9]*)', method: RequestMethod.GET },
-        { path: ':db/:docId([A-Za-b0-9]*)', method: RequestMethod.PUT },
+        // First character of ID has to be letter or number
+        // otherwise potential collision with internal endpoints (e.g. _changes, _revs_diff...)
+        { path: ':db/:docId([A-Za-z0-9].*)', method: RequestMethod.GET },
+        { path: ':db/:docId([A-Za-z0-9].*)', method: RequestMethod.PUT },
         { path: 'rules/:db/reload', method: RequestMethod.POST },
       )
       .forRoutes('*');
