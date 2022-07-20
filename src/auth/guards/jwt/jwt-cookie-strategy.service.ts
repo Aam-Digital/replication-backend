295388294644dc888b05a4cbf0ba147c0c71dc5d
Simon <simon@aam-digital.com>
2022-07-20 14:33:32 +0200

    added bearer auth JWT strategy

--- src/auth/guards/jwt/jwt-cookie-strategy.service.ts
+++ src/auth/guards/jwt/jwt-cookie-strategy.service.ts
@@ -1,34 +1,28 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import { Injectable } from '@nestjs/common';
 import { AuthModule } from '../../auth.module';
 import { User } from '../../../restricted-endpoints/session/user-auth.dto';
 import { TOKEN_KEY } from '../../cookie/cookie.service';
 import { ConfigService } from '@nestjs/config';
-import * as Sentry from '@sentry/node';
 
 /**
  * Authenticate a user using an existing JWT from a cookie in the request.
  */
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtCookieStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-cookie',
+) {
   constructor(configService: ConfigService) {
     super({
-      jwtFromRequest: (req) => {
-        const token =
-          req?.cookies[TOKEN_KEY] ||
-          ExtractJwt.fromAuthHeaderAsBearerToken()(req);
-        console.log('token', token);
-        return token;
-      },
+      jwtFromRequest: (req) => req?.cookies[TOKEN_KEY],
       ignoreExpiration: false,
       secretOrKey: configService.get<string>(AuthModule.JWT_SECRET_ENV),
     });
   }
 
   async validate(data: any): Promise<User> {
-    const user = new User(data.name, data.sub);
-    Sentry.setUser({ username: user.name });
-    return user;
+    return new User(data.name, data.sub);
   }
 }
