9d3ca15a1ecb94204ad9840b71b4bfa5a596311a
Sebastian Leidig <sebastian@aam-digital.com>
2024-03-19 17:09:46 +0100

    fix: service provider issues with AdminService

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
-import { PermissionModule } from '../permissions/permission.module';
 import { CouchdbModule } from '../couchdb/couchdb.module';
 import { AuthModule } from '../auth/auth.module';
 import { AdminService } from './admin.service';
 
 @Module({
   controllers: [AdminController],
-  imports: [PermissionModule, CouchdbModule, AuthModule],
+  imports: [CouchdbModule, AuthModule],
   providers: [AdminService],
+  exports: [AdminService],
 })
 export class AdminModule {}
