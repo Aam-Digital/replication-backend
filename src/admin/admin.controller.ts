9a376c3ecc5d7fa39c99ea2d2c7c5262d16dfd0b
Sebastian Leidig <sebastian@aam-digital.com>
2024-03-19 11:48:45 +0100

    updated code docs

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -12,16 +12,6 @@ import { AdminService } from './admin.service';
 export class AdminController {
   constructor(private adminService: AdminService) {}
 
-  /**
-   * Deletes all local documents of the remote database.
-   * These document hold meta-information about the replication process.
-   * Deleting them forces clients to re-run sync and check which documents are different.
-   * See {@link https://docs.couchdb.org/en/stable/replication/protocol.html#retrieve-replication-logs-from-source-and-target}
-   *
-   * @param db name of the database where the local documents should be deleted from
-   *
-   * This function should be called whenever the permissions change to re-trigger sync
-   */
   @Post('/clear_local/:db')
   async clearLocal(@Param('db') db: string): Promise<any> {
     await this.adminService.clearLocal(db);
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -3,10 +3,23 @@ import { firstValueFrom } from 'rxjs';
 import { AllDocsResponse } from '../restricted-endpoints/replication/bulk-document/couchdb-dtos/all-docs.dto';
 import { CouchdbService } from '../couchdb/couchdb.service';
 
+/**
+ * Service providing some general functionalities that are required in the context of administering the db and permissions.
+ */
 @Injectable()
 export class AdminService {
   constructor(private couchdbService: CouchdbService) {}
 
+  /**
+   * Deletes all local documents of the remote database.
+   * These document hold meta-information about the replication process.
+   * Deleting them forces clients to re-run sync and check which documents are different.
+   * See {@link https://docs.couchdb.org/en/stable/replication/protocol.html#retrieve-replication-logs-from-source-and-target}
+   *
+   * @param db name of the database where the local documents should be deleted from
+   *
+   * This function should be called whenever the permissions change to re-trigger sync
+   */
   async clearLocal(db: string) {
     const localDocsResponse = await firstValueFrom(
       this.couchdbService.get<AllDocsResponse>(db, '_local_docs'),
