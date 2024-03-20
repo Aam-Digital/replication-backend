7ef7f0ab000c62b6239e53d9e02da631be8d9458
Sebastian <sebastian@aam-digital.com>
2024-03-20 07:42:00 +0100

    fix: service provider issues with AdminService (#133)

--- src/permissions/permission.module.ts
+++ src/permissions/permission.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RulesService } from './rules/rules.service';
 import { PermissionService } from './permission/permission.service';
 import { HttpModule } from '@nestjs/axios';
+import { AdminModule } from '../admin/admin.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AdminModule],
   providers: [RulesService, PermissionService],
   exports: [RulesService, PermissionService],
 })
