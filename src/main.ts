3bf86695865ff291b01d6e13bc5b8be6a8c422b4
Sebastian Leidig <sebastian@aam-digital.com>
2026-04-20 12:34:56 +0200

    fix build

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import { configureSentry } from './sentry.configuration';
 import { ConfigService } from '@nestjs/config';
